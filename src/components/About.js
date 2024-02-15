20a99c7903fcbcc7054d72243c3c940dd049bd63
abhinav2697 <82115917+abhinav2697@users.noreply.github.com>
2024-02-15 23:03:16 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ class About extends Component {
     render() {
         return (
             <div className='about'>
-                <h3>About:This application provides information about the products</h3>
+                <h3>About:This Application provides information about the products</h3>
             </div>
         )
     }
